e0c50abe6898de9a64505436ced92f0e5e5eda6e
Seiji Emery <seijiemery@gmail.com>
2018-03-13 18:21:37 -0700

    finished lab
    bubblesort *NOT* optimal, but works...

--- lab_11/src/recursion.cpp
+++ lab_11/src/recursion.cpp
@@ -1,32 +1,75 @@
 #include <iostream>
+#include <cassert>
 using namespace std;
 
 int sumOfDigits(long n) 
 {
-    //TO DO
+    if (n == 0) { return 0; }
+    else { return n % 10 + sumOfDigits(n / 10); }
 }
 
 void halfHourGlass(int n)
 {
-    //TO DO
+    for (auto i = n; i --> 0; ) { std::cout << '*'; } std::cout << '\n';
+    if (n > 1) {
+        halfHourGlass(n - 1);
+        for (auto i = n; i --> 0; ) { std::cout << '*'; } std::cout << '\n';
+    }
 }
 
 //Find array of positive numbers
 int findMax(int arr[], int arrSize)
 {
-
-    //TO DO
+    assert(arrSize > 0);
+    if (arrSize == 1) {
+        return arr[arrSize-1];
+    } else {
+        return std::max(arr[arrSize-1], 
+            findMax(arr, arrSize - 1));
+    }
 }
 
 //Assume array size is at least 1
 int findMinIndex(int arr[], int arrSize)
 {
-    //TO DO
+    assert(arrSize > 0);
+    if (arrSize == 1) {
+        return arrSize - 1;
+    } else {
+        int i = arrSize - 1, j = findMinIndex(arr, i);
+        return arr[i] <= arr[j] ? i : j;
+    }
+}
+
+void displayArray (int array[], int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << array[i] << ' ';
+    }
+    std::cout << "\b\n";
+}
+
+bool bubbleSortElement (int arr[], int arrSize) {
+    if (arrSize < 2) return false;
+    bool didSwap = bubbleSortElement(arr, arrSize - 1);
+    if (arr[arrSize - 1] < arr[arrSize - 2]) {
+        std::cout << "SWAP " << (arrSize - 1) << ", " << (arrSize - 2) << '\n';
+        displayArray(arr, arrSize);
+        std::swap(arr[arrSize - 1], arr[arrSize - 2]);
+        displayArray(arr, arrSize);
+        return true;
+    }
+    return didSwap;
 }
 
 void bubbleSort(int arr[], int arrSize)
 {
-    //TO DO
+    if (arrSize > 0 && bubbleSortElement(arr, arrSize)) {
+        std::cout << "STILL NOT SORTED\n";
+        displayArray(arr, arrSize);
+        bubbleSort(arr, arrSize);
+    } else {
+        std::cout << "SORTED\n";
+    }
 }
 
 int main()
