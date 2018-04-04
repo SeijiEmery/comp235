acbc03863566c48afa1140f4876fc312056615ca
Seiji Emery <seijiemery@gmail.com>
2018-04-03 18:55:55 -0700

    finished part 1

--- lab_13/src/part1.cpp
+++ lab_13/src/part1.cpp
@@ -1,50 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 using namespace std;
 
 class LivingThing
 {
+    int m_legs = 0;
 public:
     LivingThing() {}
-    LivingThing(int p) { legs = p; }
-    int legs;
-    virtual void move() { cout << "A living thing is moving! "; };
+    LivingThing(int legs) : m_legs(legs) {}
+    virtual void move() { cout << "A living thing is moving!"; };
+    int legs () const { return m_legs; }
 };
 
 class Human : public LivingThing
 {
 public:
-    //TO DO: declare a no-arg constructor, it should call the base class
-    //  constructor using the initializer 2 for number of legs
-    
-    //TO DO: Override the 'move' function to display "Human is walking!"    
+    Human () : LivingThing(2) {}
+    void move () override { cout << "A human is walking!";}
 };
 
 class Tiger : public LivingThing
 {
 public:
-    //TO DO: declare a no-arg constructor, it should call the base class
-    //  constructor using the initializer 4 for number of legs
-    
-    //TO DO: Override the 'move' function to display "Tiger is prowling!"
-    
+    Tiger () : LivingThing(4) {}
+    void move () override { cout << "A tiger is prowling!"; }
 };
 
 int main()
 {
     cout << "Part I. Static Bindings\n";
     cout << "=======================\n";
-    //Declare an array of LivingThing base type, of size 4  
-    //Randomly populate the array with a mix of humans and tigers,
-    //simply use an initializer list.
-    //Use a loop, call the "move" virtual function from the base class
-    //and display the ‘legs’ field of each object.
+    {
+        std::vector<LivingThing> things = {
+            Human(),
+            Tiger(),
+            Human(),
+            Tiger(),
+        };
+        for (auto& thing : things) {
+            thing.move(); std::cout << " with " << thing.legs() << " legs\n";
+        }
+    }
 
     cout << "Part II. Dynamic Bindings\n";
     cout << "=========================\n";
-    //Declare an array of pointers of LivingThing base type, of size 4
-    //Randomly populate the array with a mix of pointers to humans and tigers
-    //Use a loop, call the "move" function with each LivingThing object in the
-//  array and display the ‘legs’ field of each object.
+    {
+        std::vector<std::unique_ptr<LivingThing>> things;
+        things.emplace_back(static_cast<LivingThing*>(new Human()));
+        things.emplace_back(static_cast<LivingThing*>(new Tiger()));
+        things.emplace_back(static_cast<LivingThing*>(new Human()));
+        things.emplace_back(static_cast<LivingThing*>(new Tiger()));
 
+        for (auto& thing : things) {
+            thing->move(); std::cout << " with " << thing->legs() << " legs\n";
+        }
+    }
     return 0;
 }
