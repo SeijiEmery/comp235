ad72ae247f6d5a9d043eaeb0c9a0fa4bbc89a10b
Seiji Emery <seijiemery@gmail.com>
2018-04-10 19:45:41 -0700

    finished lab 14a

--- lab_14a/src/linked_list.h
+++ lab_14a/src/linked_list.h
@@ -1,13 +1,18 @@
 // linked_list.h
-// Implements a doubly linked list
-
+// Implements a doubly linked list.
+// Note: this is for an assignment; I did not design this API.
+//
 #pragma once
+#include <iostream>
+#include <cassert>
 
 template<class T>
 class List
 {
 public:
     List();
+    List (const List&);
+    List& operator= (const List&);
     ~List();
 
     /*** getters ***/
@@ -25,11 +30,143 @@ private:
 
     struct Node
     {
-        T data;
-        Node *rlink;
+        T value;
         Node *llink;
+        Node *rlink;
+
+        Node (T value, Node* left, Node* right)
+            : value(value), llink(left), rlink(right) {}
+        Node (const Node&) = delete;
+        Node& operator= (const Node&) = delete;
+        ~Node () {}
     };
 
     Node *headPtr = nullptr;
     Node *tailPtr = nullptr;
 };
+
+template <typename T>
+List<T>::List () {}
+
+template <typename T>
+List<T>::List (const List& other) { *this = other; }
+
+template <typename T>
+List<T>& List<T>::operator= (const List<T>& other) {
+    for (Node* node = other.headPtr; node != nullptr; node = node->rlink) {
+        push_back(node->value);
+    }
+}
+
+template <typename T>
+List<T>::~List () {
+    for (Node* node = headPtr; node; ) {
+        Node* next = node->rlink;
+        delete node;
+        node = next;
+    }
+}
+
+template <typename T>
+void List<T>::removeAll (T value) {
+    for (Node* node = headPtr; node != nullptr; ) {
+        Node* next = node->rlink;
+        if (node->value == value) {
+            if (node->llink) {
+                assert(node->llink->rlink == node);
+                node->llink->rlink = node->rlink;
+            } else {
+                assert(node == headPtr);
+                headPtr = node->rlink;
+            }
+            if (node->rlink) {
+                assert(node->rlink->llink == node);
+                node->rlink->llink = node->llink;
+            } else {
+                assert(node == tailPtr);
+                tailPtr = node->llink;
+            }
+            delete node;
+        }
+        node = next;
+    }
+}
+
+template <typename T>
+void List<T>::displayListForward () {
+    bool first = true;
+    for (Node* node = headPtr; node != nullptr; node = node->rlink) {
+        std::cout 
+            << (first ? first = false, "" : ", ")
+            << node->value;
+    }
+}
+
+template <typename T>
+void List<T>::displayListBackward () {
+    bool first = true;
+    for (Node* node = tailPtr; node != nullptr; node = node->llink) {
+        std::cout 
+            << (first ? first = false, "" : ", ")
+            << node->value;
+    }
+}
+
+template <typename T>
+void List<T>::push_front (T value) {
+    Node* node = new Node(value, nullptr, headPtr);
+    if (headPtr) { 
+        assert(headPtr->llink == nullptr);
+        headPtr->llink = node;
+    }
+    assert((headPtr == nullptr) == (tailPtr == nullptr));
+    headPtr = node;
+    if (!tailPtr) { tailPtr = node; }
+}
+
+template <typename T>
+void List<T>::push_back (T value) {
+    Node* node = new Node(value, tailPtr, nullptr);
+    if (tailPtr) { 
+        assert(tailPtr->rlink == nullptr);
+        tailPtr->rlink = node;
+    }
+    assert((headPtr == nullptr) == (tailPtr == nullptr));
+    tailPtr = node;
+    if (!headPtr) { headPtr = node; }
+}
+
+template <typename T>
+void List<T>::pop_front () {
+    Node* node = headPtr;
+    if (headPtr == tailPtr) {
+        assert(node->llink == nullptr);
+        assert(node->rlink == nullptr);
+        headPtr = tailPtr = nullptr;
+    } else {
+        assert(node->rlink != nullptr);
+        assert(node->rlink->llink == node);
+        node->rlink->llink = nullptr;
+        headPtr = node->rlink;
+        assert(headPtr->llink == nullptr);
+    }
+    delete node;
+}
+
+template <typename T>
+void List<T>::pop_back () {
+    Node* node = tailPtr;
+    if (headPtr == tailPtr) {
+        assert(node->llink == nullptr);
+        assert(node->rlink == nullptr);
+        headPtr = tailPtr = nullptr;
+    } else {
+        assert(node->llink != nullptr);
+        assert(node->llink->rlink == node);
+        node->llink->rlink = nullptr;
+        tailPtr = node->llink;
+        assert(tailPtr->rlink == nullptr);
+    }
+    delete node;
+}
+
