106e04c75bbe8b090def4a4c10ce40e4c85582c2
Seiji Emery <seijiemery@gmail.com>
2018-04-19 18:49:39 -0700

    finished lab 15

--- lab_15/src/main.cpp
+++ lab_15/src/main.cpp
@@ -1,10 +1,7 @@
-// In section 17.3, the author implemented an ListIterator template class
-//  to be used with a Node template class. The Node class uses a pointer
-//  as its link field between Node objects.
+// main.cpp
+// Implements a doubly linked list and forward / reverse iterators.
+// written (rewritten?) by Seiji Emery
 //
-// I have stripped away template syntax and namespaces. I have combined all
-//  three files into one as well, to allow you to focus on the 
-//  implementation of ListIterator and its use in the main()
 
 #include <iostream>
 using namespace std;
@@ -13,107 +10,227 @@ class Node
 {
 public:
     Node() {}
-    Node(char theData, Node* theLink)
-        : data(theData), link(theLink) {}
+    Node(char data, Node* prev = nullptr, Node* next = nullptr)
+        : m_data(data), m_prev(prev), m_next(next) {}
 
-    const char& getData() const { return data; }
-    Node* getLink() { return link; }
-    void setData(const char& theData) { data = theData; }
-    void setLink(Node* pointer) { link = pointer; }
+    char const& data () const { return m_data; }
+    Node const* prev () const { return m_prev; }
+    Node const* next () const { return m_next; }
+
+    char&  data () { return m_data; }
+    Node*& prev () { return m_prev; }
+    Node*& next () { return m_next; }
 private:
-    char data = 0;
-    Node *link = nullptr;
+    char m_data = 0;
+    Node* m_prev = nullptr;
+    Node* m_next = nullptr;
 };
 
-class ListIterator
+class DequeIterator
 {
 public:
-    ListIterator() : current(nullptr) {}
-    ListIterator(Node* initial) : current(initial) {}
+    typedef DequeIterator This;
+    typedef char          value;
 
-    const char& operator *() const { return current->getData(); }
+    DequeIterator (const Node* node = nullptr) : node(node) {}
+    DequeIterator (const This& other) : node(other.node) {}
+    This& operator= (const This& other) { node = other.node; return *this; }
+    ~DequeIterator () {}
 
-    ListIterator& operator ++() { //Prefix form 
-        current = current->getLink();
+    operator bool () const { return node != nullptr; }
+
+    value operator *() const { 
+        assert(node != nullptr);
+        return node->data(); 
+    }
+    This& operator++ () {
+        assert(node != nullptr);
+        node = node->next();
         return *this;
     }
-
-    ListIterator operator ++(int) { //Postfix form 
-        ListIterator startVersion(current);
-        current = current->getLink();
-        return startVersion;
+    This& operator-- () {
+        assert(node != nullptr);
+        node = node->prev();
+        return *this;
     }
-
-    bool operator ==(const ListIterator& rightSide) const {
-        return (current == rightSide.current);
+    This& operator++ (int) {
+        This copy { *this };
+        return ++(*this), copy;
     }
-
-    bool operator !=(const ListIterator& rightSide) const {
-        return (current != rightSide.current);
+    This& operator-- (int) {
+        This copy { *this };
+        return --(*this), copy;
+    }
+    friend bool operator== (const This& a, const This& b) {
+        return (!a && !b) || a.node == b.node;
+    }
+    friend bool operator!= (const This& a, const This& b) {
+        return !(a == b);
     }
 private:
-    Node * current;
+    const Node* node = nullptr;
 };
 
-class Queue
+template <typename It>
+class ReverseIt {
+    It it;
+public:
+    typedef ReverseIt<It> This;
+    typedef typename  It::value     value;
+
+    ReverseIt (It it) : it(it) {}
+    ReverseIt (const This&) = default;
+    This& operator= (const This&) = default;
+
+    value operator* () const { return *it; }
+    This& operator++ ()    { return --it, *this; }
+    This& operator++ (int) { This copy { *this }; return ++(*this), copy; }
+    friend bool operator== (const This& a, const This& b) {
+        return a.it == b.it;
+    }
+    friend bool operator!= (const This& a, const This& b) {
+        return a.it != b.it;
+    }
+};
+
+class Deque
 {
 public:
-    typedef ListIterator Iterator;
+    typedef Deque         This;
+    typedef DequeIterator Iterator;
 
-    Queue() {}
+    Deque () {}
 
-    void add(int item) {
+    template <typename It>  Deque (It begin, It end)  { insert(begin, end); }
+    template <typename Seq> Deque (const Seq& values) { insert(values); }
+    template <typename Seq> This& operator= (const Seq& values) { return clear(), insert(values), *this; }
 
-        Node *aNewNode = new Node(item, nullptr);
- 
-        if (front == nullptr) {         
-            front = aNewNode;
-            front->setLink(nullptr);
+    Deque (This&& other) { std::swap(*this, other); }
+    This& operator= (This&& other) {
+        std::swap(front, other.front);
+        std::swap(back,  other.back);
+        return *this;
+    }
+    ~Deque () {
+        clear();
+    }
+
+    void insert (char value) {
+        pushFront(value);
+    }
+    template <typename It>
+    void insert (It begin, It end) {
+        for (; begin != end; ++begin) {
+            insert(*begin);
+        }
+    }
+    template <typename Sequence>
+    void insert (const Sequence& sequence) {
+        insert(sequence.begin(), sequence.end());
+    }
+
+    void clear () {
+        assert((front == nullptr) == (back == nullptr));
+        Node* node = front;
+        for (Node* next; node != nullptr; node = next) {
+            next = node->next();
+            delete node;
+        }
+        // assert((node == back));
+        front = back = nullptr;
+    }
+
+    operator bool () const {
+        assert((front == nullptr) == (back == nullptr));
+        return front != nullptr;
+    }
+    void pushFront (char value) {
+        front = new Node(value, nullptr, front);
+        if (Node* next = front->next()) {
+            assert(next->prev() == nullptr);
+            next->prev() = front;
+        }
+        if (back == nullptr) {
             back = front;
         }
-        else {
-            back->setLink(aNewNode);
-            back = back->getLink();          
+    }
+    void pushBack (char value) {
+        back = new Node(value, back, nullptr);
+        if (Node* prev = back->prev()) {
+            assert(prev->next() == nullptr);
+            prev->next() = back;
+        }
+        if (front == nullptr) {
+            front = back;
         }
     }
-    int remove() { Node *temp = front; front = front->getLink(); delete temp; }
 
-    bool isEmpty() const {
-        if (front == nullptr)
-            return true;
-        else
-            return false;
+    void popFront () {
+        if (Node* node = front) {
+            front = node->next();
+            delete node;
+            if (front == nullptr) {
+                back = nullptr;
+            }
+            assert((front != nullptr) == (back != nullptr));
+        }
+    }
+    void popBack () {
+        if (Node* node = back) {
+            back = node->prev();
+            delete node;
+            if (back == nullptr) {
+                front = nullptr;
+            }
+            assert((front != nullptr) == (back != nullptr));
+        }
     }
 
-    Iterator begin() const { return Iterator(front); }
-    Iterator end() const { return Iterator(); }
+    Iterator begin  () const { return Iterator(front); }
+    Iterator end    () const { return Iterator();      }
+
+    ReverseIt<Iterator> rbegin () const { return {{ back }}; }
+    ReverseIt<Iterator> rend   () const { return {{}}; }
 
 private:
-    Node * front = nullptr; //Points to the head of a linked list. 
+    Node *front = nullptr; //Points to the head of a linked list. 
                             //Items are removed at the head. 
     Node *back = nullptr; //Points to the node at the other end of 
                           //Items are added at this end.
 };
 
+template <typename Seq>
+class Reversal {
+    Seq& seq;
+public:
+    Reversal (Seq& seq) : seq(seq) {}
+    auto begin () -> decltype(seq.rbegin()) { return seq.rbegin(); }
+    auto end   () -> decltype(seq.rend())   { return seq.rend();   }
+};
+template <typename Seq>
+auto reversed (Seq& seq) -> Reversal<Seq> { return { seq }; }
+
 int main()
 {
     char next, ans;
     do {
-        Queue q;
+        Deque q;
         cout << "Enter characters:\t";
         cin.get(next);
 
         while (next != '\n')
         {
-            q.add(next);
+            q.pushBack(next);
             cin.get(next);
         }
         cout << "You entered:\t\t";
-
-        Queue::Iterator i; 
-        for (i = q.begin(); i != q.end(); i++) 
-            cout << *i;
-
+        for (auto value : q) {
+            cout << value;
+        }
+        cout << "\nReversed:   \t\t";
+        for (auto value : reversed(q)) {
+            cout << value;
+        }
         cout << endl;
         cout << "Again?(y/n): ";
         cin >> ans;
