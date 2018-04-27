d60ddb5f311d6077fc0550a6e83f689549e988f5
Seiji Emery <seijiemery@gmail.com>
2018-04-26 18:47:16 -0700

    rewrote I/O

--- lab_17/src/part2.cpp
+++ lab_17/src/part2.cpp
@@ -1,6 +1,5 @@
 //Testing functions from the algorithm header
 //Note: New syntax to you, passing functions as arguments 
-
 #include <iostream>
 #include <vector>
 #include <algorithm> 
@@ -9,43 +8,41 @@ using namespace std;
 int myRandGenerator()
 { 
     return 50 + rand() % (100 - 50 + 1); 
-}   
-
-template<class T>
-void show(T item) 
-{ 
-    cout << ' ' << item; 
 }
+
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const std::vector<T>& v) {
+    for (const auto& x : v) {
+        os << x << ' ';
+    }
+    return os;
+}
+#include "utility/util/lineio.hpp"
 
 int main()
 {
     vector<int> a(10);
     cout << "Vector size = " << a.size() << endl;
 
-    cout << "Generate\t";
-    generate(a.begin(), a.end(), myRandGenerator);
-    for_each(a.begin(), a.end(), show<int>);
-    cout << endl;
-
-    cout << "Reversed\t";
-    reverse(a.begin(), a.end());
-    for_each(a.begin(), a.end(), show<int>);
-    cout << endl;
-
-    cout << "Shuffled\t";
-    random_shuffle(a.begin(), a.end());
-    for_each(a.begin(), a.end(), show<int>);
-    cout << endl;
-
-    cout << "Sorted\t\t";
-    sort(a.begin(), a.end());
-    for_each(a.begin(), a.end(), show<int>);
-    cout << endl;   
-
-    cout << "Rotated to element 3\t";
-    rotate(a.begin(), &a[2], a.end());
-    for_each(a.begin(), a.end(), show<int>);
-    cout << endl;
-
+    {
+        generate(a.begin(), a.end(), myRandGenerator);
+        writeln() << "Generate:                 " << a;
+    }
+    {
+        reverse(a.begin(), a.end());
+        writeln() << "Reversed:                 " << a;
+    }
+    {
+        random_shuffle(a.begin(), a.end());
+        writeln() << "Shuffled:                 " << a;
+    }
+    {
+        sort(a.begin(), a.end());
+        writeln() << "Sorted:                   " << a;
+    }
+    {
+        rotate(&a[0], &a[2], &a[a.size()-1]);
+        writeln() << "Rotated (to element 3):   " << a;
+    }
     return 0;
 }
